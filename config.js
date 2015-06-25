e83abf92152ff9681722f274511cd8c99a311a69
xyclr <xyclr@163.com>
2015-06-25 20:41:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ module.exports = function() {
     return {
         appId: 'wxd8ffa8619684a575',
         appSecret: 'fbc86cc56ed5b0cad530ac2317e7933f',
-        appToken: 'weixin'
+        appToken: 'weixinjk'
     };
 };

e38b3f09908b347e59871ecbc86beac45ad897d2
xyclr <xyclr@163.com>
2015-06-25 16:04:33 +0800

    x

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = function() {
+    // 输入你的配置
+    return {
+        appId: 'wxd8ffa8619684a575',
+        appSecret: '01HCbOCSsPxYbFFlOzSUAGZBIyEK5jYUn19g6H6psci',
+        appToken: 'weixin'
+    };
+};

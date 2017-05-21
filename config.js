0195b3aa337806d79a39fba9deaf059ea83d41e3
xyclr <xyclr@163.com>
2017-05-21 17:21:56 +0800

    xxx

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = function() {
     // 输入你的配置
     return {
-        appId: 'wx0dc4996a4c1b3f2a',
-        appSecret: '1c33d19116ae8668de781a9ac108ca87',
-        appToken: 'a23mnE1mgJvN2E7PEMgJg4Z37JZL4Eww'
+        appId: 'wxd8ffa8619684a575',
+        appSecret: '0809a9cdc6877331dfe698d2f8f67518',
+        appToken: 'weixinjk'
     };
 };

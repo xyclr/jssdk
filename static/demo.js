5299c368271886d46a37174527232a4ede42e141
xyclr <xyclr@163.com>
2017-05-21 18:01:14 +0800

    xxx

--- static/demo.js
+++ static/demo.js
@@ -30,7 +30,7 @@ wx.ready(function () {
     wx.onMenuShareAppMessage({
       title: '互联网之子',
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-      link: 'http://ndesig.com/test.html#menu-share',
+      link: 'http://ndesig.com',
       imgUrl: 'http://ndesig.com/pic300.jpg',
       trigger: function (res) {
         alert('用户点击发送给朋友');
@@ -53,7 +53,7 @@ wx.ready(function () {
     wx.onMenuShareTimeline({
       title: '互联网之子',
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-      link: 'http://ndesig.com/test.html#menu-share',
+      link: 'http://ndesig.com',
       imgUrl: 'http://ndesig.com/pic300.jpg',
       trigger: function (res) {
         alert('用户点击分享到朋友圈');
@@ -76,7 +76,7 @@ wx.ready(function () {
     wx.onMenuShareQQ({
      title: '互联网之子',
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-      link: 'http://ndesig.com/test.html#menu-share',
+      link: 'http://ndesig.com',
       imgUrl: 'http://ndesig.com/pic300.jpg',
       trigger: function (res) {
         alert('用户点击分享到QQ');
@@ -102,7 +102,7 @@ wx.ready(function () {
     wx.onMenuShareWeibo({
     title: '互联网之子',
       desc: '在长大的过程中，我才慢慢发现，我身边的所有事，别人跟我说的所有事，那些所谓本来如此，注定如此的事，它们其实没有非得如此，事情是可以改变的。更重要的是，有些事既然错了，那就该做出改变。',
-      link: 'http://ndesig.com/test.html#menu-share',
+      link: 'http://ndesig.com',
       imgUrl: 'http://ndesig.com/pic300.jpg',
       trigger: function (res) {
         alert('用户点击分享到微博');
